e04ad30d77c9f567c04946fd4859a4c82e0fa5d9
Oazin <78148154+Oazin@users.noreply.github.com>
2025-06-26 19:31:53 +0200

    Add files via upload

---
+++ Run_PlantDiseaseDetection.ps1
@@ -0,0 +1,9 @@
+Write-Host "Lancement de localtunnel dans une nouvelle fenêtre..."
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "lt --port 5000 --subdomain docfarm"
+
+Start-Sleep -Seconds 3
+
+Write-Host "Lancement de l'API Flask dans une nouvelle fenêtre..."
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd 'Plant-Disease-Detection\Flask Deployed App'; python app_V2.py"
+
+
